705f96cbf2ca2f4d8dad21cc810cc26b98066345
Raymond Ho <raymonstah@gmail.com>
2021-06-15 00:25:53 -0700

    complete twitter follower app

--- functions/twitterfollower/main.go
+++ functions/twitterfollower/main.go
@@ -1,46 +1,114 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
-)
+	"regexp"
+	"strings"
 
-func main() {
-	if err := run(); err != nil {
-		panic(err)
-	}
-}
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+)
 
 // this should be relative to the main project directory.
 var humansDirectory = filepath.Join("content", "humans")
 
-func run() error {
-	existingHandles, err := getExistingFollowingHandles()
-	if err != nil {
-		return fmt.Errorf("unable to get existing twitter folowings: %w", err)
+func main() {
+	flags := flag.NewFlagSet("user-auth", flag.ExitOnError)
+	consumerKey := flags.String("consumer-key", "", "Twitter Consumer Key")
+	consumerSecret := flags.String("consumer-secret", "", "Twitter Consumer Secret")
+	accessToken := flags.String("access-token", "", "Twitter Access Token")
+	accessSecret := flags.String("access-secret", "", "Twitter Access Secret")
+	if err := flags.Parse(os.Args[1:]); err != nil {
+		log.Fatalf("error parsing flags: %v", err)
 	}
-	updatedHandles, err := getTwitterHandlesFromDir(humansDirectory)
+	if *consumerKey == "" || *consumerSecret == "" || *accessToken == "" || *accessSecret == "" {
+		log.Fatal("Consumer key/secret and Access token/secret required")
+	}
+	app := setupApp(*consumerKey, *consumerSecret, *accessToken, *accessSecret)
+	if err := app.run(); err != nil {
+		panic(err)
+	}
+
+}
+
+func setupApp(consumerKey, consumerSecret, accessToken, accessSecret string) app {
+	config := oauth1.NewConfig(consumerKey, consumerSecret)
+	token := oauth1.NewToken(accessToken, accessSecret)
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	client := twitter.NewClient(httpClient)
+
+	return app{
+		client: client,
+	}
+}
+
+type app struct {
+	client *twitter.Client
+}
+
+func (app app) run() error {
+	existingHandles, err := app.getExistingFollowingHandles()
+	if err != nil {
+		return fmt.Errorf("unable to get existing twitter followings: %w", err)
+	}
+	updatedHandles, err := app.getTwitterHandlesFromDir(humansDirectory)
 	if err != nil {
 		return fmt.Errorf("unable to get existing twitter handles: %w", err)
 	}
 	toFollow, toUnfollow := handleDiffs(existingHandles, updatedHandles)
-	if err := followHandles(toFollow); err != nil {
+	if err := app.followHandles(toFollow); err != nil {
 		return fmt.Errorf("unable to follow: %w", err)
 	}
 
-	if err := unfollowHandles(toUnfollow); err != nil {
+	if err := app.unfollowHandles(toUnfollow); err != nil {
 		return fmt.Errorf("unable to unfollow: %w", err)
 	}
 
 	return nil
 }
 
-func unfollowHandles(toUnfollow []string) error {
+func (app app) unfollowHandles(toUnfollow []string) error {
+	for _, toUnfollow := range toUnfollow {
+		fmt.Println("attempting to unfollow", toUnfollow)
+		_, resp, err := app.client.Friendships.Destroy(&twitter.FriendshipDestroyParams{
+			ScreenName: toUnfollow,
+		})
+		if err != nil {
+			return err
+		}
+		if resp.StatusCode > 300 {
+			body, _ := ioutil.ReadAll(resp.Body)
+			bodyString := string(body)
+			fmt.Println(resp.StatusCode, bodyString, resp.Header)
+		}
+	}
+
 	return nil
 }
 
-func followHandles(toFollow []string) error {
+func (app app) followHandles(toFollows []string) error {
+	for _, toFollow := range toFollows {
+		fmt.Println("attempting to follow", toFollow)
+		_, resp, err := app.client.Friendships.Create(&twitter.FriendshipCreateParams{
+			ScreenName: toFollow,
+		})
+		if err != nil {
+			return err
+		}
+		if resp.StatusCode > 300 {
+			body, _ := ioutil.ReadAll(resp.Body)
+			bodyString := string(body)
+			fmt.Println(resp.StatusCode, bodyString, resp.Header)
+		}
+	}
+
 	return nil
 }
 
@@ -50,18 +118,60 @@ func handleDiffs(existingHandles, updatedHandles []string) (toFollow, toUnfollow
 	return
 }
 
-func getExistingFollowingHandles() ([]string, error) {
-	return nil, nil
+func (app app) getExistingFollowingHandles() ([]string, error) {
+	var existingFriends []string
+	friends, _, err := app.client.Friends.List(&twitter.FriendListParams{
+		ScreenName: "aapiwiki",
+		Cursor:     -1,
+		Count:      200,
+		SkipStatus: twitter.Bool(true),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	for _, friend := range friends.Users {
+		existingFriends = append(existingFriends, friend.ScreenName)
+	}
+
+	return existingFriends, nil
 }
 
-func getTwitterHandlesFromDir(dir string) ([]string, error) {
-	wd,err := os.Getwd()
+var frontMatter = regexp.MustCompile(`twitter: (\S*)`)
+
+func (app app) getTwitterHandlesFromDir(dir string) ([]string, error) {
+	wd, err := os.Getwd()
 	if err != nil {
 		return nil, err
 	}
 	projectDirectory := filepath.Dir(filepath.Dir(wd))
-	fmt.Println("parent:", projectDirectory)
-	return nil, nil
+	humansDir := filepath.Join(projectDirectory, dir)
+
+	var usernames []string
+	err = filepath.WalkDir(humansDir, func(path string, d fs.DirEntry, err error) error {
+		if d.Name() != "index.md" {
+			return nil
+		}
+		rawContent, err := os.ReadFile(path)
+		if err != nil {
+			return fmt.Errorf("unable to read file: %w", err)
+		}
+		subgroup := frontMatter.FindStringSubmatch(string(rawContent))
+		if len(subgroup) < 2 {
+			return nil
+		}
+		twitterUsername := strings.ReplaceAll(subgroup[1], `"`, "")
+		twitterUsername = strings.TrimPrefix(twitterUsername, "https://twitter.com/")
+		if twitterUsername == "" {
+			return nil
+		}
+		usernames = append(usernames, twitterUsername)
+		return nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("unable to walk humans directory: %v: %w", humansDir, err)
+	}
+	return usernames, nil
 }
 
 // setDiff computes a - b
