e55e02eed53ec48016e78ca6714c0556a4a8e23c
Raymond Ho <raymonstah@gmail.com>
2023-01-02 21:06:31 -0800

    asian american flags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,37 @@
-<h1>AsianAmericans.wiki</h1>
+<script>
+  import countries from "../lib/flags.json";
+</script>
+
 <svelte:head>
   <title>AsianAmericans.wiki</title>
 </svelte:head>
+
+<h1>AsianAmericans.wiki</h1>
+<div class="flags">
+  {#each Object.entries(countries) as [code, country]}
+    <div class="country">
+      <span title={country.name} class="emoji">{country.emoji}</span>
+    </div>
+  {/each}
+</div>
+
+<style>
+  h1 {
+    text-align: center;
+  }
+
+  .flags {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    grid-template-columns: repeat(5, 1fr);
+  }
+
+  .country {
+    flex: 0 0 calc(16.66% - 20px);
+  }
+
+  .emoji {
+    font-size: 100px;
+  }
+</style>
