6391b2677a3190e80b481fbccb3d9115b9ebc678
Raymond Ho <raymonstah@gmail.com>
2023-05-06 13:06:28 -0700

    improve mobile viewing experience

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -51,7 +51,9 @@
   }
 </script>
 
-<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
+<nav
+  class="bg-white w-full border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900"
+>
   <div class="container flex flex-wrap items-center justify-between mx-auto">
     <a href="/" class="flex items-center">
       <img src="/scalable.svg" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
