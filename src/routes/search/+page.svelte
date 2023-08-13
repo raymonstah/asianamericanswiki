2b0e3aa245f17813a1540bd678dea6dbb984e3a3
Raymond Ho <raymonstah@gmail.com>
2023-08-13 00:48:00 -0700

    refactor human page

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -48,6 +48,7 @@
     <div
       class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
     >
+      <!-- A search icon -->
       <svg
         aria-hidden="true"
         class="w-5 h-5 text-gray-500 dark:text-gray-400"
