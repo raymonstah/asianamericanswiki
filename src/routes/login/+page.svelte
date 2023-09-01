7d5d2d05baa153d4723544458c9576b3d08a9745
Raymond Ho <raymonstah@gmail.com>
2023-08-31 17:04:56 -0700

    new google login button

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -26,5 +26,15 @@
 <svelte:head>
   <title>Login | AsianAmericans.wiki</title>
 </svelte:head>
-
-<button on:click={loginWithGoogle}>Login with Google</button>
+<button
+  class="px-4 py-2 border flex gap-2 rounded-lg text-slate-700 dark:text-slate-200 border-slate-400 hover:text-slate-900 hover:shadow transition duration-150"
+  on:click={loginWithGoogle}
+>
+  <img
+    class="w-6 h-6"
+    src="https://www.svgrepo.com/show/475656/google-color.svg"
+    loading="lazy"
+    alt="google logo"
+  />
+  <span>Login with Google</span>
+</button>
