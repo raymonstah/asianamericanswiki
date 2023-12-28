86713b164861b716e25d2dd8690deabbaf10bd73
Raymond Ho <raymonstah@gmail.com>
2023-12-27 23:43:02 -0800

    refactor middleware

--- functions/api/server/handler.go
+++ functions/api/server/handler.go
@@ -1,16 +1,11 @@
 package server
 
 import (
-	"context"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"net/http"
-	"strings"
 
-	"firebase.google.com/go/v4/auth"
 	"github.com/go-chi/httplog"
-	"github.com/raymonstah/asianamericanswiki/internal/ratelimiter"
 )
 
 type contextKey string
@@ -20,19 +15,6 @@ var (
 )
 
 type Handler func(w http.ResponseWriter, r *http.Request) error
-
-type Authorizer interface {
-	VerifyIDToken(ctx context.Context, idToken string) (*auth.Token, error)
-}
-
-type NoOpAuthorizer struct{}
-
-func (n NoOpAuthorizer) VerifyIDToken(ctx context.Context, idToken string) (*auth.Token, error) {
-	return &auth.Token{
-		UID:    "test-user",
-		Claims: map[string]interface{}{"admin": true},
-	}, nil
-}
 
 func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	oplog := httplog.LogEntry(r.Context())
@@ -52,117 +34,4 @@ func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		}
 
 	}
-}
-
-func (s *Server) OptionalAuthMiddleware(next http.Handler) http.Handler {
-	return Handler(func(w http.ResponseWriter, r *http.Request) error {
-		token, err := s.parseToken(r, true)
-		if err != nil {
-			return err
-		}
-
-		ctx := WithToken(r.Context(), token)
-		next.ServeHTTP(w, r.WithContext(ctx))
-		return nil
-	})
-
-}
-func (s *Server) AuthMiddleware(next http.Handler) http.Handler {
-	return Handler(func(w http.ResponseWriter, r *http.Request) error {
-		token, err := s.parseToken(r, false)
-		if err != nil {
-			return err
-		}
-
-		ctx := WithToken(r.Context(), token)
-		next.ServeHTTP(w, r.WithContext(ctx))
-		return nil
-	})
-
-}
-
-func (s *Server) RateLimitMiddleware(next http.Handler) http.Handler {
-	return Handler(func(w http.ResponseWriter, r *http.Request) error {
-		if err := s.rateLimiter.Check(r.RemoteAddr); err != nil {
-			if errors.Is(err, ratelimiter.ErrRateLimitExceeded) {
-				return NewTooManyRequestsError(err)
-			}
-			return NewInternalServerError(err)
-		}
-
-		next.ServeHTTP(w, r)
-		return nil
-	})
-}
-
-func (s *Server) AdminMiddleware(next http.Handler) http.Handler {
-	return Handler(func(w http.ResponseWriter, r *http.Request) error {
-		token, err := s.parseToken(r, false)
-		if err != nil {
-			return err
-		}
-
-		admin, ok := token.Claims["admin"]
-		if !ok || !admin.(bool) {
-			s.logger.Warn().Str("uid", token.UID).Msg("user is not an admin")
-			return NewForbiddenError(fmt.Errorf("user is not an admin"))
-		}
-
-		ctx := WithToken(r.Context(), token)
-		next.ServeHTTP(w, r.WithContext(ctx))
-		return nil
-	})
-}
-
-func IsAdmin(token *auth.Token) bool {
-	admin, ok := token.Claims["admin"]
-	if !ok {
-		return false
-	}
-	return admin.(bool)
-}
-
-func (s *Server) parseToken(r *http.Request, optional bool) (*auth.Token, error) {
-	ctx := r.Context()
-
-	tokenString, err := parseBearerToken(r, optional)
-	if err != nil {
-		return nil, NewUnauthorizedError(err)
-	}
-
-	if tokenString == "" && optional {
-		return nil, nil
-	}
-
-	token, err := s.authClient.VerifyIDToken(ctx, tokenString)
-	if err != nil {
-		return nil, NewUnauthorizedError(fmt.Errorf("unable to verify id token: %w", err))
-	}
-
-	return token, nil
-}
-
-func parseBearerToken(r *http.Request, optional bool) (token string, err error) {
-	tok := r.Header.Get("Authorization")
-	if len(tok) > 6 && strings.ToUpper(tok[0:7]) == "BEARER " {
-		return tok[7:], nil
-	}
-	if optional {
-		return "", nil
-	}
-	return "", fmt.Errorf("invalid authorization token")
-}
-
-// Token pulls the auth.Token out of the context.
-func Token(ctx context.Context) *auth.Token {
-	token, ok := ctx.Value(tokenKey).(*auth.Token)
-	if !ok {
-		return nil
-	}
-	return token
-}
-
-// WithToken takes a token and sticks it in the context.
-func WithToken(ctx context.Context, token *auth.Token) context.Context {
-	return context.WithValue(ctx, tokenKey, token)
 }
--- functions/api/server/handler_test.go
+++ functions/api/server/handler_test.go
@@ -2,22 +2,12 @@ package server
 
 import (
 	"bytes"
-	"context"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"net/http/httptest"
 	"os"
 	"testing"
-
-	firebase "firebase.google.com/go/v4"
-	"firebase.google.com/go/v4/auth"
-	"github.com/segmentio/ksuid"
-	"github.com/tj/assert"
-
-	"github.com/raymonstah/asianamericanswiki/functions/api"
 )
 
 func TestMain(m *testing.M) {
@@ -29,125 +19,6 @@ func TestMain(m *testing.M) {
 		log.Fatal("failed to set FIRESTORE_EMULATOR_HOST environment variable", err)
 	}
 	m.Run()
-}
-
-func TestServer_AuthMiddleware_Unauthorized(t *testing.T) {
-	ctx := context.Background()
-	app, err := firebase.NewApp(ctx, &firebase.Config{ProjectID: api.ProjectID})
-	assert.NoError(t, err)
-	authClient, err := app.Auth(ctx)
-	assert.NoError(t, err)
-
-	s := NewServer(Config{
-		AuthClient: authClient,
-	})
-
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest(http.MethodGet, "/", nil)
-	r.Header.Set("Authorization", "Bearer fakeToken")
-	h := s.AuthMiddleware(http.HandlerFunc(nil))
-
-	h.ServeHTTP(w, r)
-	body, err := ioutil.ReadAll(w.Result().Body)
-	assert.NoError(t, err)
-
-	var m map[string]interface{}
-	err = json.Unmarshal(body, &m)
-	assert.NoError(t, err)
-	assert.Contains(t, m["error"], "unable to verify id token")
-	assert.Equal(t, http.StatusUnauthorized, w.Result().StatusCode)
-}
-
-func TestServer_AuthMiddleware(t *testing.T) {
-	ctx := context.Background()
-	app, err := firebase.NewApp(ctx, &firebase.Config{ProjectID: api.ProjectID})
-	assert.NoError(t, err)
-	authClient, err := app.Auth(ctx)
-	assert.NoError(t, err)
-	s := NewServer(Config{
-		AuthClient: authClient,
-	})
-
-	email := fmt.Sprintf("%v@test.com", ksuid.New().String())
-	password := ksuid.New().String()
-	userRecord, err := authClient.CreateUser(ctx, (&auth.UserToCreate{}).Email(email).Password(password))
-	assert.NoError(t, err)
-	defer func() {
-		err := authClient.DeleteUser(ctx, userRecord.UID)
-		assert.NoError(t, err)
-	}()
-
-	idToken, err := signIn(email, password)
-	assert.NoError(t, err)
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest(http.MethodGet, "/", nil)
-	r.Header.Set("Authorization", "Bearer "+idToken)
-	h := s.AuthMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		token := Token(r.Context())
-		assert.NotNil(t, token)
-		assert.Equal(t, userRecord.UID, token.UID)
-	}))
-
-	h.ServeHTTP(w, r)
-	assert.Equal(t, http.StatusOK, w.Result().StatusCode)
-}
-
-func TestServer_AdminMiddleware(t *testing.T) {
-	ctx := context.Background()
-	app, err := firebase.NewApp(ctx, &firebase.Config{ProjectID: api.ProjectID})
-	assert.NoError(t, err)
-	authClient, err := app.Auth(ctx)
-	assert.NoError(t, err)
-	s := NewServer(Config{
-		AuthClient: authClient,
-	})
-
-	email := fmt.Sprintf("%v@test.com", ksuid.New().String())
-	password := ksuid.New().String()
-	userRecord, err := authClient.CreateUser(ctx, (&auth.UserToCreate{}).Email(email).Password(password))
-	assert.NoError(t, err)
-
-	// Give user the admin claim
-	claims := map[string]any{"admin": true}
-	err = authClient.SetCustomUserClaims(ctx, userRecord.UID, claims)
-	assert.NoError(t, err)
-
-	defer func() {
-		err := authClient.DeleteUser(ctx, userRecord.UID)
-		assert.NoError(t, err)
-	}()
-
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest(http.MethodGet, "/", nil)
-	h := s.AdminMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		token := Token(r.Context())
-		assert.NotNil(t, token)
-		assert.Equal(t, userRecord.UID, token.UID)
-	}))
-
-	idToken, err := signIn(email, password)
-	assert.NoError(t, err)
-	r.Header.Set("Authorization", "Bearer "+idToken)
-	h.ServeHTTP(w, r)
-	assert.Equal(t, http.StatusOK, w.Result().StatusCode)
-
-	t.Run("without-admin-claim", func(t *testing.T) {
-		// Remove the admin claim
-		err := authClient.SetCustomUserClaims(ctx, userRecord.UID, nil)
-		assert.NoError(t, err)
-
-		idToken, err = signIn(email, password)
-		assert.NoError(t, err)
-		r.Header.Set("Authorization", "Bearer "+idToken)
-
-		w := httptest.NewRecorder()
-		h.ServeHTTP(w, r)
-		assert.Equal(t, http.StatusForbidden, w.Result().StatusCode)
-		bodyRaw, err := ioutil.ReadAll(w.Result().Body)
-		assert.NoError(t, err)
-
-		assert.JSONEq(t, `{"error":"user is not an admin"}`, string(bodyRaw))
-	})
 }
 
 func signIn(email, password string) (string, error) {
