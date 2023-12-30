653f467bae2f27fe6739e6c246a077f7ba6d428f
Raymond Ho <raymonstah@gmail.com>
2023-12-29 22:28:37 -0800

    add GA

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import preprocess from "svelte-preprocess";
 import adapter from "@sveltejs/adapter-node";
+import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,6 +11,7 @@ const config = {
     preprocess({
       postcss: true,
     }),
+    vitePreprocess(),
   ],
   extensions: [".svelte"],
 };
