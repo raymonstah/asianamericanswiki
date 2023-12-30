4dbd37840e14a2dcb8422243913c13be6b395401
Raymond Ho <raymonstah@gmail.com>
2023-12-29 22:28:37 -0800

    add GA

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 <script>
   import Header from "$lib/components/Header.svelte";
   import Footer from "$lib/components/Footer.svelte";
+  import Analytics from "$lib/components/Analytics.svelte";
   import "$lib/styles/style.css";
   import "../app.postcss";
 </script>
 
 <Header />
 
+<Analytics />
 <main>
   <slot />
 </main>
