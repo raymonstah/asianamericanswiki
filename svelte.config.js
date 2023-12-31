e6e597e22264579cf486e332e9823c840ff20544
Raymond Ho <raymonstah@gmail.com>
2023-12-31 14:46:38 -0800

    upgrade svelte+vite

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import preprocess from "svelte-preprocess";
 import adapter from "@sveltejs/adapter-node";
-import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +10,6 @@ const config = {
     preprocess({
       postcss: true,
     }),
-    vitePreprocess(),
   ],
   extensions: [".svelte"],
 };
