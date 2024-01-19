bfbe65c0a45fa145a818a4eaa6f2f29027f66ed1
Raymond Ho <raymonstah@gmail.com>
2024-01-19 00:27:49 -0800

    rollbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,22 @@
   import Analytics from "$lib/components/Analytics.svelte";
   import "$lib/styles/style.css";
   import "../app.postcss";
+  import { dev } from "$app/environment";
+
+  // include and initialize the rollbar library with your access token
+  import Rollbar from "rollbar";
+  var mode = "prod";
+  if (dev) {
+    mode = "dev";
+  }
+  var rollbar = new Rollbar({
+    accessToken: "e1082079233c44628d29032fc1847ca7",
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+    payload: {
+      environment: mode,
+    },
+  });
 </script>
 
 <Header />
