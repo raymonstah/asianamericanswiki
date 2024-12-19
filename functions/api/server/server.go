26f08c335cc7b88e3bdb8e53801db961dd313373
Raymond Ho <raymonstah@gmail.com>
2024-12-18 21:12:25 -0800

    tweak edit page

--- functions/api/server/server.go
+++ functions/api/server/server.go
@@ -93,6 +93,7 @@ func (s *Server) setupRoutes() {
 		r.Use(httplog.RequestLogger(s.logger))
 		r.Route("/api/v1/", func(r chi.Router) {
 			r.Method(http.MethodGet, "/version", Handler(s.Version))
+			r.Method(http.MethodGet, "/humans", Handler(s.Humans))
 		})
 	})
 }
@@ -104,6 +105,77 @@ func (s *Server) Version(w http.ResponseWriter, r *http.Request) error {
 	}
 	s.writeData(w, http.StatusOK, data)
 	return nil
+}
+
+type Human struct {
+	ID          string          `json:"id"`
+	Name        string          `json:"name"`
+	Path        string          `json:"path"`
+	DOB         string          `json:"dob"`
+	DOD         string          `json:"dod"`
+	Tags        []string        `json:"tags"`
+	Ethnicity   []string        `json:"ethnicity"`
+	Image       string          `json:"image"`
+	Description string          `json:"description"`
+	Socials     HumanSocial     `json:"socials"`
+	Views       int64           `json:"views"`
+	Gender      humandao.Gender `json:"gender"`
+}
+
+type HumanSocial struct {
+	Instagram string `json:"instagram"`
+	X         string `json:"x"`
+	Website   string `json:"website"`
+	IMDB      string `json:"imdb"`
+}
+
+func (s *Server) Humans(w http.ResponseWriter, r *http.Request) error {
+	ctx := r.Context()
+	humansRaw, ok := s.humanCache.Get("humans")
+	if ok {
+		s.logger.Debug().Msg("cache hit")
+		s.writeData(w, http.StatusOK, humansRaw)
+		return nil
+	}
+	humans, err := s.humanDAO.ListHumans(ctx, humandao.ListHumansInput{
+		Limit:  500,
+		Offset: 0,
+	})
+	if err != nil {
+		s.writeData(w, http.StatusInternalServerError, err)
+	}
+	humansResult := convert(humans)
+	s.humanCache.Set("humans", humansResult, cache.DefaultExpiration)
+	s.writeData(w, http.StatusOK, humans)
+	return nil
+}
+
+func convert(humans []humandao.Human) []Human {
+	var humansOut []Human
+	for _, human := range humans {
+		humanOut := Human{
+			ID:          human.ID,
+			Name:        human.Name,
+			Path:        human.Path,
+			DOB:         human.DOB,
+			DOD:         human.DOD,
+			Tags:        human.Tags,
+			Ethnicity:   human.Ethnicity,
+			Image:       human.FeaturedImage,
+			Description: human.Description,
+			Socials: HumanSocial{
+				Instagram: human.Socials.Instagram,
+				X:         human.Socials.X,
+				Website:   human.Socials.Website,
+				IMDB:      human.Socials.IMDB,
+			},
+			Views:  human.Views,
+			Gender: human.Gender,
+		}
+		humansOut = append(humansOut, humanOut)
+	}
+
+	return humansOut
 }
 
 func (s *Server) writeData(w http.ResponseWriter, status int, data any) {
