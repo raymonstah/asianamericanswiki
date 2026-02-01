4ecff3201d558a90e7ef2a15a2a47f0cb1d06cad
Raymond Ho <raymonstah@gmail.com>
2026-01-31 20:24:08 -0800

    fix proto

--- functions/api/server/api.pb.gw.go
+++ functions/api/server/api.pb.gw.go
@@ -10,6 +10,7 @@ package server
 
 import (
 	"context"
+	"errors"
 	"io"
 	"net/http"
 
@@ -24,64 +25,71 @@ import (
 )
 
 // Suppress "imported and not used" errors
-var _ codes.Code
-var _ io.Reader
-var _ status.Status
-var _ = runtime.String
-var _ = utilities.NewDoubleArray
-var _ = metadata.Join
+var (
+	_ codes.Code
+	_ io.Reader
+	_ status.Status
+	_ = errors.New
+	_ = runtime.String
+	_ = utilities.NewDoubleArray
+	_ = metadata.Join
+)
 
 func request_HumanService_Version_0(ctx context.Context, marshaler runtime.Marshaler, client HumanServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq VersionRequest
-	var metadata runtime.ServerMetadata
-
+	var (
+		protoReq VersionRequest
+		metadata runtime.ServerMetadata
+	)
+	if req.Body != nil {
+		_, _ = io.Copy(io.Discard, req.Body)
+	}
 	msg, err := client.Version(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
-
 }
 
 func local_request_HumanService_Version_0(ctx context.Context, marshaler runtime.Marshaler, server HumanServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq VersionRequest
-	var metadata runtime.ServerMetadata
-
+	var (
+		protoReq VersionRequest
+		metadata runtime.ServerMetadata
+	)
 	msg, err := server.Version(ctx, &protoReq)
 	return msg, metadata, err
-
 }
 
 func request_HumanService_Humans_0(ctx context.Context, marshaler runtime.Marshaler, client HumanServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq HumansRequest
-	var metadata runtime.ServerMetadata
-
+	var (
+		protoReq HumansRequest
+		metadata runtime.ServerMetadata
+	)
+	if req.Body != nil {
+		_, _ = io.Copy(io.Discard, req.Body)
+	}
 	msg, err := client.Humans(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
-
 }
 
 func local_request_HumanService_Humans_0(ctx context.Context, marshaler runtime.Marshaler, server HumanServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq HumansRequest
-	var metadata runtime.ServerMetadata
-
+	var (
+		protoReq HumansRequest
+		metadata runtime.ServerMetadata
+	)
 	msg, err := server.Humans(ctx, &protoReq)
 	return msg, metadata, err
-
 }
 
 // RegisterHumanServiceHandlerServer registers the http handlers for service HumanService to "mux".
 // UnaryRPC     :call HumanServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
 // Note that using this registration option will cause many gRPC library features to stop working. Consider using RegisterHumanServiceHandlerFromEndpoint instead.
+// GRPC interceptors will not work for this type of registration. To use interceptors, you must use the "runtime.WithMiddlewares" option in the "runtime.NewServeMux" call.
 func RegisterHumanServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux, server HumanServiceServer) error {
-
-	mux.Handle("GET", pattern_HumanService_Version_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle(http.MethodGet, pattern_HumanService_Version_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		var stream runtime.ServerTransportStream
 		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		var err error
-		var annotatedContext context.Context
-		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/main.HumanService/Version", runtime.WithHTTPPathPattern("/v1/version"))
+		annotatedContext, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/main.HumanService/Version", runtime.WithHTTPPathPattern("/v1/version"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -93,20 +101,15 @@ func RegisterHumanServiceHandlerServer(ctx context.Context, mux *runtime.ServeMu
 			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
 			return
 		}
-
 		forward_HumanService_Version_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
 	})
-
-	mux.Handle("GET", pattern_HumanService_Humans_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle(http.MethodGet, pattern_HumanService_Humans_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		var stream runtime.ServerTransportStream
 		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		var err error
-		var annotatedContext context.Context
-		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/main.HumanService/Humans", runtime.WithHTTPPathPattern("/v1/humans"))
+		annotatedContext, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/main.HumanService/Humans", runtime.WithHTTPPathPattern("/v1/humans"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -118,9 +121,7 @@ func RegisterHumanServiceHandlerServer(ctx context.Context, mux *runtime.ServeMu
 			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
 			return
 		}
-
 		forward_HumanService_Humans_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
 	})
 
 	return nil
@@ -147,7 +148,6 @@ func RegisterHumanServiceHandlerFromEndpoint(ctx context.Context, mux *runtime.S
 			}
 		}()
 	}()
-
 	return RegisterHumanServiceHandler(ctx, mux, conn)
 }
 
@@ -161,16 +161,13 @@ func RegisterHumanServiceHandler(ctx context.Context, mux *runtime.ServeMux, con
 // to "mux". The handlers forward requests to the grpc endpoint over the given implementation of "HumanServiceClient".
 // Note: the gRPC framework executes interceptors within the gRPC handler. If the passed in "HumanServiceClient"
 // doesn't go through the normal gRPC flow (creating a gRPC client etc.) then it will be up to the passed in
-// "HumanServiceClient" to call the correct interceptors.
+// "HumanServiceClient" to call the correct interceptors. This client ignores the HTTP middlewares.
 func RegisterHumanServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux, client HumanServiceClient) error {
-
-	mux.Handle("GET", pattern_HumanService_Version_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle(http.MethodGet, pattern_HumanService_Version_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		var err error
-		var annotatedContext context.Context
-		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/main.HumanService/Version", runtime.WithHTTPPathPattern("/v1/version"))
+		annotatedContext, err := runtime.AnnotateContext(ctx, mux, req, "/main.HumanService/Version", runtime.WithHTTPPathPattern("/v1/version"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -181,18 +178,13 @@ func RegisterHumanServiceHandlerClient(ctx context.Context, mux *runtime.ServeMu
 			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
 			return
 		}
-
 		forward_HumanService_Version_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
 	})
-
-	mux.Handle("GET", pattern_HumanService_Humans_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle(http.MethodGet, pattern_HumanService_Humans_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		var err error
-		var annotatedContext context.Context
-		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/main.HumanService/Humans", runtime.WithHTTPPathPattern("/v1/humans"))
+		annotatedContext, err := runtime.AnnotateContext(ctx, mux, req, "/main.HumanService/Humans", runtime.WithHTTPPathPattern("/v1/humans"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -203,22 +195,17 @@ func RegisterHumanServiceHandlerClient(ctx context.Context, mux *runtime.ServeMu
 			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
 			return
 		}
-
 		forward_HumanService_Humans_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
 	})
-
 	return nil
 }
 
 var (
 	pattern_HumanService_Version_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "version"}, ""))
-
-	pattern_HumanService_Humans_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "humans"}, ""))
+	pattern_HumanService_Humans_0  = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "humans"}, ""))
 )
 
 var (
 	forward_HumanService_Version_0 = runtime.ForwardResponseMessage
-
-	forward_HumanService_Humans_0 = runtime.ForwardResponseMessage
+	forward_HumanService_Humans_0  = runtime.ForwardResponseMessage
 )
